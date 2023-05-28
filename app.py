3062ab3f0d23e56226fecf721320731916172046
Poorshad <p.shaddel@gmail.com>
2023-05-28 13:27:14 +0200

    add other functionalities in todo application

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 app = Flask(__name__)
@@ -15,9 +15,47 @@ class Todo (db.Model):
     def __repr__ (self):
         return '<Task %r>' % self.id
 
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    return render_template('index.html')
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = Todo (content=task_content)
 
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was an error adding the task'
+    else:
+        tasks = Todo.query.order_by(Todo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+@app.route ('/tasks/delete/<int:id>', methods=['POST'])
+def delete(id):
+    print('delete')
+    print(id)
+    task_to_delete = Todo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was a problem deleting that task'
+
+@app.route ('/tasks/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        print('update')
+        print(id)
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return 'There was a problem updating that task'
+    else:
+        return render_template('update.html', task=task)
 if __name__ == '__main__':
      app.run(port=8000, debug=True)
